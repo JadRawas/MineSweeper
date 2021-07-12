8f9fe7efc1bc930f9218b461571987f857b2b593
Jad Rawas <jadorawas@gmail.com>
2021-07-12 21:14:34 +0300

    Add project files.

---
+++ MineSweeper/MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int row = 10;
+        int column = 10;
+        int mine = 25;
+        Button[,] buttonlist;
+        MineSweeperC Game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            newGame();
+           
+        }
+
+        private void right_click(object sender, MouseButtonEventArgs e)
+        {
+            var source = e.Source as FrameworkElement;
+            if (source != null)
+            {
+                string elementName = source.Name;
+                int[] xy = getCoordinates(elementName);
+                flag(xy[0], xy[1]);
+            }
+        }
+
+
+        //Click Listeners
+        private void Field_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var source = e.OriginalSource as FrameworkElement;
+            if (source == null)
+                return;
+            int[] xy = getCoordinates(source.Name);
+
+            if (Game.playerAtion_chooseGrid(xy[0], xy[1], true))
+            {
+                player_lost();
+                MessageBox.Show("player lost");
+                newGame();
+                return;
+            }
+            Update_Field();
+            if (Game.Won())
+            {
+                MessageBox.Show("Player Won!!");
+                newGame();
+            }
+            
+        }
+
+        //Place or remove flag
+        public void flag(int x, int y)
+        {
+            bool[,] isFlagged = Game.getIsFlagged();
+            if (isFlagged[x, y])
+            {
+                isFlagged[x, y] = false;
+                buttonlist[x, y].Content = "";
+                buttonlist[x, y].Foreground = Brushes.Black;
+                Game.remainingMines++;
+            }
+            else
+            {
+                isFlagged[x, y] = true;
+                buttonlist[x, y].Content = "🚩";
+                buttonlist[x, y].Foreground = Brushes.Red;
+                Game.remainingMines--;
+            }
+            this.Title = "Mines left: " +Game.remainingMines;
+        }
+
+
+        //Update Interface
+        private void Update_Field()
+        {
+            bool[,] revealed = Game.getRevealed();
+            int[,] numbers = Game.getNumbers();
+            for(int i = 0; i < row; i++)
+            {
+                for(int j = 0; j < column; j++)
+                {
+                    if (revealed[i, j])
+                    {
+                       buttonlist[i, j].IsEnabled = false;
+                       buttonlist[i, j].Content = numbers[i, j];
+                       buttonlist[i, j].Foreground = Brushes.Black;
+                        if (numbers[i, j] == 0)
+                            buttonlist[i, j].Content = "";
+                    }
+                }
+            }
+        }
+        //reveal All
+        public void player_lost()
+        {
+            bool[,] revealed = Game.getRevealed();
+            int[,] numbers = Game.getNumbers();
+            bool[,] mineLoc = Game.getMineLocation();
+            bool[,] isFlagged = Game.getIsFlagged();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (mineLoc[i, j])
+                    {
+                        buttonlist[i, j].IsEnabled = false;
+                        buttonlist[i, j].Content = "💣";
+                        buttonlist[i,j].Foreground = Brushes.Black;
+                        if (isFlagged[i, j])
+                        {
+                            buttonlist[i,j].Foreground = Brushes.Green;
+                        }
+
+                    }
+                    else
+                    {
+                        buttonlist[i, j].IsEnabled = false;
+                        buttonlist[i, j].Content = numbers[i, j];
+                        buttonlist[i, j].Foreground = Brushes.Black;
+                        if(numbers[i, j]==0)
+                             buttonlist[i, j].Content = "";
+                        if (isFlagged[i, j])
+                        {
+                            buttonlist[i, j].Foreground = Brushes.Red;
+                            buttonlist[i, j].Content = numbers[i, j]+"🚩";
+                        }
+                    }
+                }
+            }
+        }
+        /*
+        New Game
+        */ 
+        public void newGame()
+        {
+            Field.Children.Clear();
+            Field.RowDefinitions.Clear();
+            Field.ColumnDefinitions.Clear();
+            buttonlist = new Button[row, column];
+
+            //Setting Interface
+            for (int i = 0; i < column; i++)
+            {
+                ColumnDefinition C = new ColumnDefinition();
+                Field.ColumnDefinitions.Add(C);
+            }
+            for (int i = 0; i < row; i++)
+            {
+                RowDefinition R = new RowDefinition();
+                Field.RowDefinitions.Add(R);
+            }
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    Button B = new Button();
+                    B.Name = "r" + i + "c" + j;
+                    B.MouseDown += new MouseButtonEventHandler(right_click);
+                    //B.Content = i + "," + j;
+                    buttonlist[i, j] = B;
+                    Grid.SetColumn(B, j);
+                    Grid.SetRow(B, i);
+                    Field.Children.Add(B);
+                }
+            }
+            //Initializing game
+            Game = new MineSweeperC(row, column, mine);
+            this.Title = "Mines left: " + Game.remainingMines;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            row = 5;
+            column = 5;
+            mine = 6;
+            newGame();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            row = 10;
+            column = 10;
+            mine = 25;
+            newGame();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            row = 15;
+            column = 15;
+            mine = 60;
+            newGame();
+        }
+
+        /*
+        utility
+        */
+
+        //get's the name of the button and return it's coordinates
+        private int[] getCoordinates(String s)
+        {
+            int[] xy = new int[2];
+            String x = "";
+            String y = "";
+
+            int i;
+            for(i = 1; s[i] != 'c'; i++)
+            {
+                x = x + s[i];
+            }
+            for(i = i+1; i < s.Length; i++)
+            {
+                y = y + s[i];
+            }
+            xy[0] = Int32.Parse(x);
+            xy[1] = Int32.Parse(y);
+            return xy;
+        }
+    }
+}
---
+++ MineSweeper/MineSweeper.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MineSweeper
+{
+    class MineSweeperC
+    {
+        private int rows;                    //no need to explain
+        private int columns;                 //no nead to explain this either
+        private int bombs;                   //number of bombs (should be less than the total ammount of grid by 9)
+        private bool isFirst;                //true until the player picks a grid for the 1st time
+        private int[,] gameBoard;            //contain the number of neighboring grids that contains a mine
+        private Boolean[,] containsMine;     //true if the grid contains a mine
+        private Boolean[,] revealed;         //true if the grid was already clicked by the player
+        private Boolean[,] isFlagged;        //true if the grid was flagged
+        public int remainingMines;          //numbers of mines - number of flags
+        private Boolean playerLost;          //true if the player lost
+
+
+        public MineSweeperC(int rows, int columns, int bombs)
+        {
+            isFirst = true;
+            this.rows = rows;
+            this.columns = columns;
+            this.bombs = bombs;
+            playerLost = false;
+            remainingMines = bombs;
+
+            gameBoard = new int[rows, columns];
+            revealed = new Boolean[rows, columns];
+            isFlagged = new Boolean[rows, columns];
+            containsMine = new Boolean[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    containsMine[i, j] = false;
+                    revealed[i, j] = false;
+                    isFlagged[i, j] = false;
+                }
+            }
+        }
+
+
+        /*
+        player actions
+        */
+        //return true If the player clicked on a mine, false otherwise.
+        public bool playerAtion_chooseGrid(int x, int y, bool isPlayer)
+        {
+            if (x == -1 || x == rows || y == -1 || y == columns)
+                return false;
+            if (isFlagged[x, y] && isPlayer)
+            {
+                return false;
+            }
+            if (isFirst)
+            {
+                isFirst = false;
+                setValueOfNeighborTo(x, y, -1);
+                placeMines();
+                setValueOfNeighborTo(x, y, 0);
+                setNumbers();
+                playerAtion_chooseGrid(x, y, false);
+                return false;
+            }
+            if (revealed[x, y])
+            {
+                return false;
+            }
+            if (containsMine[x, y])
+            {
+                playerLost = true;
+                return true;
+            }
+            else
+            {
+                revealed[x, y] = true;
+                if (gameBoard[x, y] == 0)
+                {
+                    playerAtion_chooseGrid(x-1, y-1, false);
+                    playerAtion_chooseGrid(x-1, y+1, false);
+                    playerAtion_chooseGrid(x-1, y, false);
+                    playerAtion_chooseGrid(x, y-1, false);
+                    playerAtion_chooseGrid(x, y+1, false);
+                    playerAtion_chooseGrid(x+1, y-1, false);
+                    playerAtion_chooseGrid(x+1, y+1, false);
+                    playerAtion_chooseGrid(x+1, y, false);
+                }
+                return false;
+            }
+            
+           
+
+           
+        }
+
+        //returns 0 if: flag wasn't placed. 1 if: the flag was placed. 2 if: the flag was removed
+        public int playerAtion_addOrRemoveFlag(int x, int y)
+        {
+            if (revealed[x, y])
+                return 0;
+            if (isFlagged[x, y])
+            {
+                isFlagged[x, y] = false;
+                remainingMines++;
+                return 2;
+            }
+            isFlagged[x, y] = true;
+            remainingMines--;
+            return 1;
+        }
+
+
+
+
+        /*
+        Setters
+        */
+        public void setNumbers()
+        {
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    if (containsMine[i, j])
+                    {
+                        addValueToNeighbor(i, j, 1);
+                    }
+                }
+            }
+        }
+
+        public void placeMines()
+        {
+            for (int i = 0; i < bombs; i++)
+            {
+                Random rand = new Random();
+                while (true)
+                {
+                    int x = rand.Next(0, rows);
+                    int y = rand.Next(0, columns);
+                    if (!containsMine[x,y] && gameBoard[x,y]!=-1)
+                    {
+                        containsMine[x,y] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+
+
+        /*
+        getters 
+        */
+        public Boolean Lost()
+        {
+            return playerLost;
+        }
+        public int[,] getNumbers()
+        {
+            return gameBoard;
+        }
+        public Boolean[,] getRevealed()
+        {
+            return revealed;
+        }
+        public Boolean[,] getMineLocation()
+        {
+            return containsMine;
+        }
+        public Boolean[,] getIsFlagged()
+        {
+            return isFlagged;
+        }
+
+        public Boolean Won()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (!containsMine[i, j] && !revealed[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+
+        /*
+        Utility functions
+        */
+        public void setGridValueTo(int x, int y, int value)
+        {
+            try
+            {
+                gameBoard[x, y] = value;
+            } catch (IndexOutOfRangeException)
+            {
+                return;
+            }
+            
+        }
+        public void addValueAtGrid(int x, int y, int value)
+        {
+            try
+            {
+                gameBoard[x, y] += value;
+            } catch(IndexOutOfRangeException)
+            {
+                return;
+            }
+        }
+
+        public void setValueOfNeighborTo(int x, int y, int value)
+        {
+            setGridValueTo(x, y, value);
+            setGridValueTo(x, y + 1, value);
+            setGridValueTo(x, y - 1, value);
+
+            setGridValueTo(x - 1, y, value);
+            setGridValueTo(x - 1, y + 1, value);
+            setGridValueTo(x - 1, y - 1, value);
+
+            setGridValueTo(x + 1, y, value);
+            setGridValueTo(x + 1, y - 1, value);
+            setGridValueTo(x + 1, y + 1, value);
+        }
+        public void addValueToNeighbor(int x, int y, int value)
+        {
+            addValueAtGrid(x, y, value);
+            addValueAtGrid(x, y + 1, value);
+            addValueAtGrid(x, y - 1, value);
+
+            addValueAtGrid(x - 1, y, value);
+            addValueAtGrid(x - 1, y + 1, value);
+            addValueAtGrid(x - 1, y - 1, value);
+
+            addValueAtGrid(x + 1, y, value);
+            addValueAtGrid(x + 1, y - 1, value);
+            addValueAtGrid(x + 1, y + 1, value);
+        }
+
+
+    }
+}
